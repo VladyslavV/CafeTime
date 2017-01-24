acab8bef5acd2d24eccdd2376777d4cb86ab1e44
Vladysalv Vyshnevksyy <vvyshnevskyy@waverleysoftware.com>
2017-01-24 17:43:07 +0200

    start

---
+++ CafeTime/CafeTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CafeTime
+//
+//  Created by Vladysalv Vyshnevksyy on 1/24/17.
+//  Copyright © 2017 Vladysalv Vyshnevksyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
