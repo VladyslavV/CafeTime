6fd7e21d9d9bef705f4531b3c8ededc838008111
Vladysalv Vyshnevksyy <vvyshnevskyy@waverleysoftware.com>
2017-02-07 17:21:46 +0200

    localized strings

--- CafeTime/CafeTime/UIViewController+Extension.swift
+++ CafeTime/CafeTime/UIViewController+Extension.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 extension UIViewController {
-
+    
+    // error
     func presentAlert(message: String) {
-        let alertController = UIAlertController(title: NSLocalizedString("vc.extenstion.allert", comment: ""), message: message, preferredStyle: .alert)
-        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        let alertController = UIAlertController(title: NSLocalizedString("allert.title.error", comment: ""), message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: NSLocalizedString("allert.button.ok", comment: ""), style: .cancel, handler: nil)
         alertController.addAction(defaultAction)
         present(alertController, animated: true, completion: nil)
     }
@@ -22,18 +23,20 @@ extension UIViewController {
         vcs[0].present(nav, animated: true, completion: nil)
     }
     
+    // enter credentials
     func presenetAlertWithCredentialFields(completion: @escaping (String,String) -> () ) {
         
-        let alert = UIAlertController(title: "Enter Credentials", message: nil, preferredStyle: .alert)
+        let alert = UIAlertController(title: NSLocalizedString("allert.enter.credentials", comment: ""), message: nil, preferredStyle: .alert)
+        
         alert.addTextField { (textField) in
-            textField.placeholder = "Email"
+            textField.placeholder = NSLocalizedString("allert.email.textfield", comment: "")
         }
         alert.addTextField { (textField) in
-            textField.placeholder = "Password"
+            textField.placeholder = NSLocalizedString("allert.password.textfield", comment: "")
             textField.isSecureTextEntry = true
         }
         
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+        alert.addAction(UIAlertAction(title: NSLocalizedString("allert.button.ok", comment: ""), style: .default, handler: { [weak alert] (_) in
             let textFieldEmail = alert?.textFields![0]
             let textFieldPassword = alert?.textFields![01]
             
@@ -42,7 +45,7 @@ extension UIViewController {
             }
         }))
 
-        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("allert.button.cancel", comment: ""), style: .default, handler: nil))
         
         self.present(alert, animated: true, completion: nil)
     }
